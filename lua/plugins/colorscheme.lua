91b9b3c2d67badbb9be6fa75a27f6e232dbff434
Youcef Mez <mezyoucef32@gmail.com>
2023-11-14 17:30:08 +0100

    cleanup & refactoring

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,30 +1,55 @@
 return {
   -- add gruvbox
-  { "ellisonleao/gruvbox.nvim" },
-  -- { "nyoom-engineering/oxocarbon.nvim" },
-  -- { "rebelot/kanagawa.nvim" },
-  -- { "sainnhe/sonokai" },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = true,
+    opts = function()
+      return {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "hard", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+    end,
+  },
   -- { "tanvirtin/monokai.nvim" },
   -- Configure LazyVim to load gruvbox
-  -- { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
   { "savq/melange-nvim" },
-  -- {"cpea2506/one_monokai.nvim"},
   { "Mofiqul/vscode.nvim" },
-  { "catppuccin/nvim" },
+  -- { "catppuccin/nvim" },
   -- { "shaunsingh/solarized.nvim" },
-  { "maxmx03/solarized.nvim" },
-  -- { "AstroNvim/astrotheme" },
-  -- { "oxfist/night-owl.nvim" },
-  -- { "Tsuzat/NeoSolarized.nvim" },
-  -- { "neanias/everforest-nvim" },
-  -- { "ray-x/starry.nvim" },
-  -- { "kepano/flexoki-neovim" },
+  -- { "maxmx03/solarized.nvim" },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
   {
     "LazyVim/LazyVim",
     opts = {
-      -- colorscheme = "flexoki-light",
+      colorscheme = "gruvbox",
       -- colorscheme = "melange",
-      colorscheme = "solarized",
+      -- colorscheme = "tokyonight",
       -- colorscheme = "night-owl",
       -- transparent = true,
     },
