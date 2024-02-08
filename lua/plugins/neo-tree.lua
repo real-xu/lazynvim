9c8fac81c3b1a0e0e49d8556277a505963e3afdb
Luke Shay <38476395+lukeshay@users.noreply.github.com>
2024-02-08 09:27:46 -0600

    Create neo-tree.lua

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    config = function()
+      require("neo-tree").setup({
+          window = {
+            position = "right"
+          }
+        })
+    end
+  }
+}
