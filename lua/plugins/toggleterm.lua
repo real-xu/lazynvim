0597e55a532d67982c8c4085f3846939756900cf
lefv <luis@lefv.io>
2024-02-18 13:39:52 -0500

    fixed toggleterm.

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,7 +1,6 @@
 return {
   -- amongst your other plugins
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
+  { "akinsho/toggleterm.nvim", version = "*", config = true },
   -- or
-  {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
+  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
 }
-
