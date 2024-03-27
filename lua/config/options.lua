98cfe8617f1b557d88977837c8c4abe53e0a778e
Luke Shay <robert.shay@advantagesolutions.net>
2024-03-27 16:23:25 -0500

    clear snippets on normal mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+function leave_snippet()
+  if
+    ((vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n") or vim.v.event.old_mode == "i")
+    and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+    and not require("luasnip").session.jump_active
+  then
+    require("luasnip").unlink_current()
+  end
+end
+
+-- stop snippets when you leave to normal mode
+vim.api.nvim_command([[
+    autocmd ModeChanged * lua leave_snippet()
+]])
