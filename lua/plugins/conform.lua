fe109e01dec460e61ec1a3358092a7053b443f18
shepherdwind <eward.song@gmail.com>
2024-04-05 14:26:53 +0800

    feat: enable copilot by LazyExtras and disable json auto format

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "eslint_d" },
+        typescript = { "eslint_d" },
+        typescriptreact = { "eslint_d" },
+      },
+    },
+  },
+}
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,3 +1,4 @@
 return {
   -- { "folke/trouble.nvim", enabled = false }
+  { "L3MON4D3/LuaSnip", enabled = false },
 }
