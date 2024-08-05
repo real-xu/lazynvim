f2fbad0885b16e132bf65cde13bf872e999964d3
lefv <luis.fernandez-de-la-vara@c3.ai>
2024-08-05 13:32:34 -0400

    Adding vscode config.

--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,29 +1,19 @@
-local lazypath = "/Users/lefv/test/vscode/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
-vim.opt.rtp:prepend(lazypath)
-local vscode = require("vscode-neovim")
--- Execute _ping synchronously and print the result
-print(vscode.call("_ping")) -- outputs: pong
-
--- Wait for 1 second and print the return value 'ok'
-print(vscode.call("_wait", { args = { 1000 } })) -- outputs: ok
-
--- Wait for 2 seconds with a timeout of 1 second
-print(vscode.call("_wait", { args = { 2000 } }), 1000)
--- error: Call '_wait' timed out
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
-    -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -38,19 +28,89 @@ require("lazy").setup({
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = false }, -- automatically check for plugin updates
-  -- performance = {
-  --   rtp = {
-  --     -- disable some rtp plugins
-  --     disabled_plugins = {
-  --       "gzip",
-  --       -- "matchit",
-  --       -- "matchparen",
-  --       -- "netrwPlugin",
-  --       "tarPlugin",
-  --       "tohtml",
-  --       "tutor",
-  --       "zipPlugin",
-  --     },
-  --   },
-  -- },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
 })
+
+
+-- Format current document
+-- vscode.action("editor.action.formatDocument");
+
+-- do -- Find in files for word under cursor
+--   vscode.action("workbench.action.findInFiles", {
+--     args = { query = vim.fn.expand('<cword>') }
+--   })
+-- end
+
+------------------
+--- has_config ---
+------------------
+
+-- Check if the configuration "not.exist" exists
+
+-- Should return: false
+
+-- -- Check multiple configurations
+-- vim.print("Multiple configurations? " + vscode.has_config({ "not.exist", "existing.config" }))
+
+-- -- Get the value of "editor.tabSize"
+-- print("Current tab size? " + vscode.get_config("editor.tabSize")) -- a number
+
+-- -- Get multiple configurations
+
+-- Should return: { "the font family", "the editor tabSizse" }
+
+-- Should return: { false, true }
+-- error: Call '_wait' timed out
+
+-- require("lazy").setup({
+--   spec = {
+--     -- add LazyVim and import its plugins
+--     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+--     -- import any extras modules here
+--     { import = "lazyvim.plugins.extras.lang.typescript" },
+--     { import = "lazyvim.plugins.extras.lang.json" },
+--     { import = "lazyvim.plugins.extras.ui.mini-animate" },
+--     -- import/override with your plugins
+--     { import = "plugins" },
+--   },
+--   defaults = {
+--     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+-- default.
+--     lazy = false,
+--     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+--     -- have outdated releases, which may break your Neovim install.
+--     -- version = false, -- always use the latest git commit
+--     version = "*", -- try installing the latest stable version for plugins that support semver
+--   },
+--   install = { colorscheme = { "tokyonight", "habamax" } },
+--   checker = { enabled = false }, -- automatically check for plugin updates
+--   -- performance = {
+--   --   rtp = {
+--   --     -- disable some rtp plugins
+--   --     disabled_plugins = {
+--   --       "gzip",
+--   --       -- "matchit",
+--   --       -- "matchparen",
+--   --       -- "netrwPlugin",
+--   --       "tarPlugin",
+--   --       "tohtml",
+--   --       "tutor",
+--   --       "zipPlugin",
+--   --     },
+--   --   },
+--   -- },
+-- })
