a08d4386fce0ee5b8f6eb014e3181baedc22c3b3
flin16 <flin16@illinois.edu>
2025-05-25 15:21:30 -0500

    Finalizing setup

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,5 +1,12 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
+local excluded_list = {
+  "*.pdf", "*.aux", "*.bbl", "*.bcf", "*.blg", "*.fdb_latexmk", "*.fls", "*.log", "*.pdf", "*.run%.xml",
+  "*.synctex%.gz", "*.out", "*.toc", "*.pyc", "*.pyo", "*.xml", "*.gz", "*.DS_Store", "*.zip",
+  "__pycache__",
+  "venv",
+  ".git",
+}
 return {
   {
     'tomasr/molokai', -- colorscheme
@@ -8,72 +15,77 @@ return {
     end,
   },
   {
-      "lervag/vimtex",
-      lazy = false,     -- we don't want to lazy load VimTeX
-      init = function()
-        vim.g.vimtex_view_method = "skim"
-        vim.g.vimtex_view_skim_sync = 1
-        vim.g.vimtex_view_skim_activate = 1
-      end
+    "lervag/vimtex",
+    lazy = false, -- we don't want to lazy load VimTeX
+    init = function()
+      vim.g.vimtex_view_method = "skim"
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
+    end
   },
-  {"mfussenegger/nvim-lint",
-  opts = {
-    linters_by_ft = {
-      sh = { "shellcheck" },
-      tex = { "chktex" },
-    },
-  },
-  config = function()
-      local lint = require("lint")
-      lint.linters.ruff.args = { "--ignore=E401", }
-  end
-  },
-  {"stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      python = { "black" },
-      sh = { "shfmt" },
-      lua = { "/opt/homebrew/bin/lua" },
-      tex = { "latexindent" },
-    },
-  }},
-  {"neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      pyright = {
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        sh = { "shellcheck" },
+        tex = { "chktex" },
       },
     },
-  }},
-  {
-  "mfussenegger/nvim-dap-python",
-  ft = "python",
-  dependencies = {
-    "mfussenegger/nvim-dap",
-  },
-  config = function()
-    require("dap-python").setup("python")
-  end,
+    config = function()
+      local lint = require("lint")
+      lint.linters.ruff.args = { "--ignore=E401", }
+    end
   },
   {
-   "amitds1997/remote-nvim.nvim",
-   dependencies = {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "black" },
+        sh = { "shfmt" },
+        lua = { "/opt/homebrew/bin/lua" },
+        tex = { "latexindent" },
+      },
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+        },
+      },
+    }
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    config = function()
+      require("dap-python").setup("python")
+    end,
+  },
+  {
+    "amitds1997/remote-nvim.nvim",
+    dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       "nvim-telescope/telescope.nvim",
-   },
-   config = true,
+    },
+    config = true,
   },
   {
-  "CopilotC-Nvim/CopilotChat.nvim",
-  dependencies = {
-    { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-    { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
-  },
-  build = "make tiktoken", -- Only on MacOS or Linux
-  opts = {
-    -- See Configuration section for options
-  },
-  -- See Commands section for default commands if you want to lazy load on them
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" },                       -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",                          -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
   },
   {
     "folke/snacks.nvim",
@@ -83,34 +95,20 @@ return {
           files = {
             hidden = true,
             ignored = true,
-            exclude = {
-              "*.pdf",
-              "*.aux",
-              "*.bbl",
-              "*.bcf",
-              "*.blg",
-              "*.fdb_latexmk",
-              "*.fls",
-              "*.log",
-              "*.pdf",
-              "*.run%.xml",
-              "*.synctex%.gz",
-              "*.out",
-              "*.toc",
-              "*.pyc",
-              "*.pyo",
-              "*.xml",
-              "*.gz",
-              "*.DS_Store",
-              "*.zip",
-              "__pycache__",
-              "venv",
-              ".git",
-            },
-            },
+            exclude = excluded_list,
           },
+          explorer = {
+            exclude = excluded_list,
+          }
+        },
       },
     }
+  },
+  {
+    "mbbill/undotree",
+    config = function()
+      vim.keymap.set("n", "<leader>U", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
+    end,
   }
 }
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
