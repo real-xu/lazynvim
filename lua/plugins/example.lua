0d5bd8fe3403ca47c914de0caa76a65fdbed9171
flin16 <flin16@illinois.edu>
2025-06-04 18:01:56 -0500

    Remove timeout
    a series mistake in the config

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -209,15 +209,14 @@ return {
     version = false, -- Never set this value to "*"! Never!
     opts = {
       -- add any opts here
-	hints = { enabled = false },
+      hints = { enabled = false },
       -- for example
       provider = "openai",
       providers = {
         openai = {
           endpoint = "https://api.openai.com/v1",
-          model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
+          model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
           extra_request_body = {
-            timeout = 30000,            -- Timeout in milliseconds, increase this for reasoning models
             temperature = 0.75,
             max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
@@ -234,12 +233,12 @@ return {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
-      "echasnovski/mini.pick",       -- for file_selector provider mini.pick
+      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
       "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp",            -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua",            -- for file_selector provider fzf
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua",      -- for providers='copilot'
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
