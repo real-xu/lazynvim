2c753cb9cd739f93b759113567efe07954c8a698
flin16 <flin16@illinois.edu>
2025-06-12 15:41:01 -0500

    feat(plugins): add JSON language support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.o.clipboard = ""
+
+if vim.fn.has("gui_running") == 1 then
+  vim.o.clipboard = ""
+end
 vim.o.guifont = "Monaco:h16"
 vim.cmd("set wrap")
 local dap = require("dap")
