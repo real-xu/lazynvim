315f9f753beff894fe90192f7106747add761473
flin16 <flin16@illinois.edu>
2025-06-14 18:30:52 -0500

    feat(lsp): add custom diagnostics for 'really' usage

--- init.lua
+++ init.lua
@@ -1,4 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
