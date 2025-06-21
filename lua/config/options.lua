0e22b208236889fb26bf19f7f364891bc48aa979
fengyuanlin <flin16@illinois.edu>
2025-06-21 13:49:15 -0500

    Add wsl clipboard support
    in options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,6 +4,19 @@
 
 if vim.fn.has("gui_running") == 1 then
   vim.o.clipboard = ""
+elseif vim.fn.has("wsl") == 1 then
+  vim.g.clipboard = {
+    name = "WSL clipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = "powershell.exe -NoProfile -Command Get-Clipboard",
+      ["*"] = "powershell.exe -NoProfile -Command Get-Clipboard",
+    },
+    cache_enabled = 0,
+  }
 end
 vim.g.copilot_no_tab_map = true
 vim.o.guifont = "Monaco:h16"
